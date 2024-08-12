cdcbe7075fdb321c587120830ccb54bfe4a51945
dev4ox <dev4ox@gmail.com>
2024-08-13 02:47:15 +0300

    before connect payments

--- regSaitTest/registration/urls.py
+++ regSaitTest/registration/urls.py
@@ -20,12 +20,12 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('orders/<int:order_id>/get_ticket/', views.generate_or_get_pdf, name='order_get_tickets'),
     path('orders/<int:order_id>/orders_details', views.order_details, name='orders_details'),
-    # path('orders/pay/', views.order_pay, name='orders_pay'),
+    path('orders/pay/', views.order_pay, name='orders_pay'),
     # path('orders/<int:order_id>/pay/', views.order_pay, name='orders_pay'),
 
     # payments
     # path('payments/', views.payments, name='payments'),
-    path('payments/', views.initiate_payment, name='initiate_payment'),
+    path('payments', views.initiate_payment, name='initiate_payment'),
     # path('payment/callback/', views.payment_callback, name='payment_callback'),
 
     # admin
